47c2e89ab3481a2c740a9bc3e4e48aac3cee98dc
leakec <leakec57@gmail.com>
2022-07-17 10:36:48 -0700

    Fixing this so directories are not created that need not be.

--- push_configs.sh
+++ push_configs.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.vim/ && cp vimrc ~/.vim/vimrc
 mkdir -p ~/.config/awesome/ && cp theme.lua ~/.config/awesome/ && cp rc.lua ~/.config/awesome/
-mkdir -p ~/.config/picom/picom.conf && cp picom.conf ~/.config/picom/picom.conf
+mkdir -p ~/.config/picom && cp picom.conf ~/.config/picom/picom.conf
 cp gromit-mpx.cfg ~/.config/gromit-mpx.cfg
 cp tmux.conf ~/.tmux.conf
 cat ./gterminal.preferences | dconf load /org/gnome/terminal/legacy/profiles:/
@@ -10,4 +10,4 @@ cp InputAutoCfg.ini /usr/local/share/mupen64plus/InputAutoCfg.ini
 cp ./xscreensaver ~/.xscreensaver
 cp ./zshrc ~/.zshrc
 cp ./ctags ~/.ctags
-cp -r nvim ~/.config/nvim
+rm -rf ~/.config/nvim; cp -r nvim ~/.config/nvim
