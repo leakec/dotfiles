baf9a5cfba876b9314a708650c484c1686e99a7d
leakec <leakec57@gmail.com>
2022-08-07 10:49:43 -0700

    Now using awesome-copycats for customization.

--- pull_configs.sh
+++ pull_configs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cp ~/.vim/vimrc .
-cp ~/.config/awesome/* .
+cp ~/.config/awesome/theme.lua .
+cp ~/.config/awesome/rc.lua .
 cp ~/.config/picom/picom.conf .
 cp ~/.config/gromit-mpx.cfg .
 cp ~/.tmux.conf ./tmux.conf
--- push_configs.sh
+++ push_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/.vim/ && cp vimrc ~/.vim/vimrc
-mkdir -p ~/.config/awesome/ && cp theme.lua ~/.config/awesome/ && cp rc.lua ~/.config/awesome/
+mkdir -p ~/.config/awesome/ && cp theme.lua ~/.config/awesome/ && cp rc.lua ~/.config/awesome/ && cp -r awesome-copycats/* ~/.config/awesome
 mkdir -p ~/.config/picom && cp picom.conf ~/.config/picom/picom.conf
 cp gromit-mpx.cfg ~/.config/gromit-mpx.cfg
 cp tmux.conf ~/.tmux.conf
