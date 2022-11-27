d3044ff541fe633d86e13208ef3762d1039de089
leakec <leakec57@gmail.com>
2022-11-27 09:18:07 -0800

    Fixing typos and removing unecessary lines.

--- push_configs.sh
+++ push_configs.sh
@@ -3,7 +3,6 @@
 mkdir -p ~/.vim/ && cp vimrc ~/.vim/vimrc
 mkdir -p ~/.config/awesome/ && cp theme.lua ~/.config/awesome/ && cp rc.lua ~/.config/awesome/ && cp -r awesome-copycats/* ~/.config/awesome
 mkdir -p ~/.config/picom && cp picom.conf ~/.config/picom/picom.conf
-mkdir -p ~/.config/alacritty/ && cp alacritty.yml ~/.config/alacritty/alacritty.yml
 cp gromit-mpx.cfg ~/.config/gromit-mpx.cfg
 cp tmux.conf ~/.tmux.conf
 cp InputAutoCfg.ini /usr/local/share/mupen64plus/InputAutoCfg.ini
@@ -12,7 +11,8 @@ cp ./zshrc ~/.zshrc
 cp ./ctags ~/.ctags
 rm -rf ~/.config/nvim; cp -r nvim ~/.config/nvim
 cp screenkey.json ~/.config/screenkey.json
-rm -rf ~/.confg/kitty; cp -r kitty ~/.config/kitty
+rm -rf ~/.config/kitty; cp -r kitty ~/.config/kitty
 
 # Not used anymore
 # cat ./gterminal.preferences | dconf load /org/gnome/terminal/legacy/profiles:/
+#mkdir -p ~/.config/alacritty/ && cp alacritty.yml ~/.config/alacritty/alacritty.yml
