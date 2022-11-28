d2d48053a6a63f78c965fb7ce6202e1795fce41d
leakec <leakec57@gmail.com>
2022-11-28 11:00:00 -0800

    Fixing input delay in tmux. * Fixing minor errors in nvim/init.lua.

--- nvim/init.lua
+++ nvim/init.lua
@@ -79,7 +79,7 @@ require('telescope').setup {
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
--- require('telescope').load_extension('fzf')
+require('telescope').load_extension('fzf')
 
 -- Install packer for plugins
 local fn = vim.fn
