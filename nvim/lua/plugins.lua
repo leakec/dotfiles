c06cfbc7abca0a3c840f905b1bf8068b133c107e
leakec <leakec57@gmail.com>
2024-01-11 16:19:29 -0800

    Adding plugin to navigate the undo tree.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,6 +18,16 @@ return {
         },
     },
 
+    -- Navigating the undo tree
+    {
+        "jiaoshijie/undotree",
+        dependencies = "nvim-lua/plenary.nvim",
+        config = true,
+        keys = { -- load the plugin only when using it's keybinding:
+          { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+        },
+    },
+
     -- Copy-paste into osc52
     {'ojroques/nvim-osc52'},
 
