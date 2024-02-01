e1af0cdb5f5b0ee38f29715cb4bd9c20eb4757d0
leakec <leakec57@gmail.com>
2024-02-01 14:36:19 -0800

    Now using treesitter context.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,24 @@
 -- Tree sitter for code folding
 return {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    ensure_installed = { "cpp", "python" },
-    auto_install = true,
-    config = function()
-        require('nvim-treesitter.configs').setup {
-            auto_install = true,
-        }
-    end
+    {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        ensure_installed = { "cpp", "python" },
+        auto_install = true,
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                auto_install = true,
+            }
+        end
+    },
+
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        config = function()
+            local tc = require("treesitter-context")
+            tc.setup({enable=false})
+            vim.keymap.set("n", "<leader>gc", function() tc.go_to_context(vim.v.count1) end, { silent = true })
+            vim.keymap.set("n", "<leader>tc", function() vim.cmd.TSContextToggle() end, { silent = true })
+        end
+    }
 }
