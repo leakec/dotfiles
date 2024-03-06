fd156e51cc3a20f01a1252afb8c6ea74856f61d0
leakec <leakec57@gmail.com>
2024-03-06 08:48:56 -0800

    Adding some keymaps and using yamroot as project directory if it exists.

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -5,10 +5,25 @@ return
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+        settings = {
+            key = function()
+                -- Try to use yamroot if it exists. Otherwise, use vim.loop.cwd
+                local plenary_path = require("plenary.path")
+                local handle = io.popen("yamroot")
+                local output = handle:read()
+                print(output)
+                handle:close()
+                path = plenary_path:new(output)
+                if path:exists() then
+                    return path.path
+                else
+                    return vim.loop.cwd()
+                end
+            end,
+        }
         local harpoon = require("harpoon")
-        harpoon.setup({})
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        harpoon.setup({settings=settings})
+
         -- basic telescope configuration
         local conf = require("telescope.config").values
         local function toggle_telescope(harpoon_files)
@@ -26,7 +41,25 @@ return
                 sorter = conf.generic_sorter({}),
             }):find()
         end
+
+        -- Creating keymaps
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         vim.keymap.set("n", "<leader>fh", function() toggle_telescope(harpoon:list()) end,
         { desc = "Open harpoon window" })
+
+        -- Adding keymaps in the UI
+        harpoon:extend({
+            UI_CREATE = function(cx)
+                vim.keymap.set("n", "1", function() harpoon:list():select(1) end)
+                vim.keymap.set("n", "2", function() harpoon:list():select(2) end)
+                vim.keymap.set("n", "3", function() harpoon:list():select(3) end)
+                vim.keymap.set("n", "4", function() harpoon:list():select(4) end)
+
+                vim.keymap.set("n", "<C-v>", function()
+                    harpoon.ui:select_menu_item({ vsplit = true })
+                end, { buffer = cx.bufnr })
+            end,
+})
     end
 }
