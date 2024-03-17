b97d52b6dbdf394b785de09f2cfe080836ebdc73
leakec <leakec57@gmail.com>
2024-03-17 08:20:04 -0700

    Adding git root as potential harpoon directory.

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,16 +7,26 @@ return
     config = function()
         settings = {
             key = function()
-                -- Try to use yamroot if it exists. Otherwise, use vim.loop.cwd
+                -- Try to use yamroot if it exists.
                 local plenary_path = require("plenary.path")
                 local handle = io.popen("yamroot")
                 local output = handle:read()
                 handle:close()
-                path = plenary_path:new(output)
+                local path = plenary_path:new(output)
                 if path:exists() then
                     return path.path
                 else
-                    return vim.loop.cwd()
+                    local handle_git = io.popen("git rev-parse --show-toplevel")
+                    local output_git = handle:read()
+                    handle_git:close()
+                    local path_git = plenary_path:new(output_git)
+                    if path_git:exists() then
+                        -- Use the git root if it exists
+                        return path_git.path
+                    else
+                        -- Fallback to vim.loop.cwd() otherwise
+                        return vim.loop.cwd()
+                    end
                 end
             end,
         }
