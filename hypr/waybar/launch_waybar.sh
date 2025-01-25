69ca8fee5b999e937cef9c703caf3d8486f1a382
leakec <leakec57@gmail.com>
2025-01-25 10:24:46 -0800

    Adding the ability to change waybar layouts via rofi.

--- hypr/waybar/launch_waybar.sh
+++ hypr/waybar/launch_waybar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONFIG_FILES="$HOME/.config/hypr/waybar/config.txt $HOME/.config/hypr/waybar/style.css $HOME/.cache/wal/colors-waybar.css"
+CONFIG_FILES="$HOME/.config/hypr/waybar/configs $HOME/.config/hypr/waybar/style.css $HOME/.cache/wal/colors-waybar.css"
 
 trap "killall waybar" EXIT
 STATE_FILE=$HOME/.config/hypr/waybar/waybar_state.txt
@@ -8,7 +8,7 @@ STATE_FILE=$HOME/.config/hypr/waybar/waybar_state.txt
 while true; do
     echo "" > $STATE_FILE
     \cp -r $HOME/.cache/wal/colors-waybar.css $HOME/.config/hypr/waybar &
-    waybar -c $HOME/.config/hypr/waybar/config.txt -s $HOME/.config/hypr/waybar/style.css &
-    inotifywait -e create,modify $CONFIG_FILES
+    waybar -c $HOME/.config/hypr/waybar/config -s $HOME/.config/hypr/waybar/style.css &
+    inotifywait -e attrib $CONFIG_FILES
     killall waybar
 done
