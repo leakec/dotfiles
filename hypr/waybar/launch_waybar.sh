10a40b08ffa6cd28e55d97753f3b8d8dba2201e1
leakec <leakec57@gmail.com>
2025-01-25 10:35:16 -0800

    Now creating symlink if it doesn't exist.

--- hypr/waybar/launch_waybar.sh
+++ hypr/waybar/launch_waybar.sh
@@ -5,6 +5,11 @@ CONFIG_FILES="$HOME/.config/hypr/waybar/configs $HOME/.config/hypr/waybar/style.
 trap "killall waybar" EXIT
 STATE_FILE=$HOME/.config/hypr/waybar/waybar_state.txt
 
+config_file=$HOME/.config/hypr/waybar/config
+if [ ! -f $config_file ]; then
+    ln -sf $HOME/.config/hypr/waybar/configs/main $config_file
+fi
+
 while true; do
     echo "" > $STATE_FILE
     \cp -r $HOME/.cache/wal/colors-waybar.css $HOME/.config/hypr/waybar &
