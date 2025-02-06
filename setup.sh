6c0c46a0b6b300612b103fe99e5d36547d0aa0a3
leake <carl.leake@karanadyn.com>
2025-02-06 07:39:05 -0700

    Adding -s flag to actually sync. Without it we just do a test.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,41 @@ if [ ! -L "~/.config/hypr/monitors.conf" ]; then
     ln -rs ~/.config/hypr/laptop_monitors.conf ~/.config/hypr/monitors.conf
 fi
 
-for pkg in ${PKGS[@]}; do
-    stow --adopt -nv $pkg
+show_help() {
+    echo "Usage: $0 [-h] [-s]"
+    echo ""
+    echo "Options:"
+    echo "  -h    Show this help message"
+    echo "  -s    Sync the configs"
+}
+
+# Default values
+sync_flag=0
+
+# Parse command-line options
+while getopts ":hs" opt; do
+    case ${opt} in
+        h )
+            show_help
+            exit 0
+            ;;
+        s )
+            sync_flag=1
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" >&2
+            show_help
+            exit 1
+            ;;
+    esac
 done
+
+if [ $sync_flag -eq 1 ]; then
+    for pkg in ${PKGS[@]}; do
+        stow --adopt $pkg
+    done
+else
+    for pkg in ${PKGS[@]}; do
+        stow --adopt -nv $pkg
+    done
+fi
