134c751d172def428639eb244bbdd0546b6d93c0
leakec <leakec57@gmail.com>
2025-04-27 07:35:28 -0700

    Fixing how this works to use the symlinks of stow correctly.

--- hypr/.config/hypr/waybar/launch_waybar.sh
+++ hypr/.config/hypr/waybar/launch_waybar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONFIG_FILES="$HOME/.config/hypr/waybar/configs $HOME/.config/hypr/waybar/style.css $HOME/.cache/wal/colors-waybar.css"
+CONFIG_FILES="$HOME/.config/hypr/waybar/config $HOME/.config/hypr/waybar/configs $HOME/.config/hypr/waybar/style.css $HOME/.cache/wal/colors-waybar.css"
 
 trap "killall waybar" EXIT
 STATE_FILE=$HOME/.config/hypr/waybar/waybar_state.txt
@@ -19,6 +19,6 @@ while true; do
     echo "" > $STATE_FILE
     cp -r $HOME/.cache/wal/colors-waybar.css $HOME/.config/hypr/waybar
     waybar -c $HOME/.config/hypr/waybar/config -s $HOME/.config/hypr/waybar/style.css &
-    inotifywait -e attrib,modify $CONFIG_FILES
+    inotifywait -P -e attrib,modify $CONFIG_FILES
     killall waybar
 done
