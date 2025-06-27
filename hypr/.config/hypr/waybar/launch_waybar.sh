48342041459a6c950ab18f323b0466959ed48a09
leakec <leakec57@gmail.com>
2025-06-27 06:35:01 -0700

    Finally fixing this (I think).

--- hypr/.config/hypr/waybar/launch_waybar.sh
+++ hypr/.config/hypr/waybar/launch_waybar.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-CONFIG_FILES="$HOME/.config/hypr/waybar/config $HOME/.config/hypr/waybar/configs $HOME/.config/hypr/waybar/style.css $HOME/.cache/wal/colors-waybar.css"
+CONFIG_FILES=(
+    "$HOME/.config/hypr/waybar/config"
+    "$HOME/.config/hypr/waybar/configs"
+    "$HOME/.config/hypr/waybar/style.css"
+    "$HOME/.cache/wal/colors-waybar.css"
+)
 
 trap "killall waybar" EXIT
 STATE_FILE=$HOME/.config/hypr/waybar/waybar_state.txt
@@ -15,10 +20,24 @@ if [ ! -f $style_file ]; then
     ln -sf $HOME/.config/hypr/waybar/styles/main.css $style_file
 fi
 
+trap 'echo "Received SIGINT"; exit 1' INT
+
+# Function to start waybar and track its PID
+start_waybar() {
+    echo "" > "$STATE_FILE"
+    cp -r $HOME/.cache/wal/colors-waybar.css $HOME/.config/hypr/waybar/
+    setsid waybar -c $HOME/.config/hypr/waybar/config -s $HOME/.config/hypr/waybar/style.css &
+    WAYBAR_PID=$!
+}
+
+# Function to stop waybar
+cleanup() {
+    [[ -n "$WAYBAR_PID" ]] && kill "$WAYBAR_PID"
+}
+
 while true; do
-    echo "" > $STATE_FILE
-    cp -r $HOME/.cache/wal/colors-waybar.css $HOME/.config/hypr/waybar
-    waybar -c $HOME/.config/hypr/waybar/config -s $HOME/.config/hypr/waybar/style.css &
-    inotifywait -P -e attrib,modify $CONFIG_FILES
-    killall waybar
+    start_waybar
+    inotifywait -P -e modify,attrib ${CONFIG_FILES[@]}
+    cleanup
+    sleep 0.1
 done
