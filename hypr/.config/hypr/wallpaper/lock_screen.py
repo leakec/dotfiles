66ca6516c87f89f441ef2f2c94e8b866e3b0a622
leakec <leakec57@gmail.com>
2025-12-15 15:20:29 -0800

    Switching to hypridle/hyprlock.

--- hypr/.config/hypr/wallpaper/lock_screen.py
+++ hypr/.config/hypr/wallpaper/lock_screen.py
@@ -1,3 +1,4 @@
+import os
 from subprocess import Popen
 from pathlib import Path
 pic_file = Path("/tmp/swww_pics.txt")
@@ -8,4 +9,7 @@ with open(pic_file,"r") as f:
     counter = (int(lines[-1])-1)%(len(lines)-1)
     pic = lines[counter].strip()
 
-Popen(["swaylock","-i",pic])
+env = os.environ
+env["WALLPAPER"] = pic
+
+Popen(["hyprlock"],env=env)
