1344a99056f2c051c63403b29a4f1e9e12729162
leake <leakec57@gmail.com>
2026-01-16 10:39:03 -0800

    Working on adding quotes.

--- hypr/.config/hypr/wallpaper/lock_screen.py
+++ hypr/.config/hypr/wallpaper/lock_screen.py
@@ -1,6 +1,13 @@
 import os
 from subprocess import Popen
 from pathlib import Path
+from random import choice
+
+quotes = [
+    "I'm afraid to move for fear of getting some of the world on me. - Alana (Sergio De La Pava)",
+    "Do not pray for an easy life, pray for the strength to endure a difficult one - Bruce Lee",
+]
+
 pic_file = Path("/tmp/swww_pics.txt")
 
 
@@ -11,5 +18,6 @@ with open(pic_file,"r") as f:
 
 env = os.environ
 env["WALLPAPER"] = pic
+env["QUOTE"] = choice(quotes)
 
 Popen(["hyprlock"],env=env)
