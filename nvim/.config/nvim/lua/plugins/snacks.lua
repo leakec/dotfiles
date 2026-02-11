15e254f63b443948afcd113266a6aa2e18728cb7
leake <carl.leake@karanadyn.com>
2026-02-11 16:57:09 -0700

    Removing neoscroll in favor of snacks.scroll.

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -14,7 +14,7 @@ return {
         picker = { enabled = true },
         notifier = { enabled = true },
         quickfile = { enabled = true },
-        -- scroll = { enabled = true },
+        scroll = { enabled = true },
         -- statuscolumn = { enabled = true },
         words = { enabled = false },
     },
