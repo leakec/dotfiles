8f4cba2833fe661b5905017fe7fec161fb0452a1
leake <carl.leake@karanadyn.com>
2026-03-02 10:41:27 -0700

    Updating to use ruff rather than pydocstyle.

--- nvim/.config/nvim/lua/plugins/neogen-config.lua
+++ nvim/.config/nvim/lua/plugins/neogen-config.lua
@@ -20,6 +20,26 @@ return {
         }
 
         vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate()<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<Leader>ncf", ":cexpr system(\"python -m pydocstyle | paste - - | sed -n 's|^\\\\(.*\\\\):\\\\([0-9]\\\\+\\\\).* \\\\(D[0-9]\\\\+.*\\\\)|\\\\1:\\\\2:0:\\\\3|p'\")<CR>", { noremap = true, silent = true })
+
+        vim.keymap.set("n", "<Leader>ncf", function()
+            -- Run yamroot and trim newline
+            local root = vim.fn.system("yamroot"):gsub("%s+$", "")
+            local config_path = root ~= "" and (root .. "/common/ruff.toml") or nil
+            local cmd
+
+            if config_path and vim.fn.filereadable(config_path) == 1 then
+                cmd = "ruff check "
+                .. " --config "
+                .. vim.fn.shellescape(config_path)
+                .. " --output-format=concise"
+            else
+                cmd = "ruff check "
+                .. " --select D --output-format=concise"
+            end
+
+            -- Use :cexpr so quickfix opens automatically
+            vim.cmd("cexpr system(\"" .. cmd .. "\")")
+        end, { desc = "Ruff docstring check (quickfix)" })
+
     end
 }
